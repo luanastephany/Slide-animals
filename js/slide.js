c7def0ece0140469db8867b810b658426f4cac51
Luana Stephany <ls.luanastephany@gmail.com>
2021-07-12 15:52:18 -0300

    iniciando parte do slide

--- js/slide.js
+++ js/slide.js
@@ -2,18 +2,39 @@ export default class Slide {
   constructor(slide, wrapper) {
     this.slide = document.querySelector(slide)
     this.wrapper = document.querySelector(wrapper)
+    this.dist = {
+      finalPosition: 0,
+      startX: 0, //referência inicial
+      movement: 0
+    }
+  }
+
+  moveSlide(distX) {
+    this.dist.movePosition = distX
+    this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
+  }
+
+  //salvar a posição pro slide não voltar pra posição inicial
+  updatePosition(clientX) {
+    this.dist.movement = this.dist.startX - clientX
+    return this.dist.finalPosition - this.dist.movement
   }
 
   onStart(event) {
     event.preventDefault()
+    this.dist.startX = event.clientX
+    console.log(this.dist.startX)
     this.wrapper.addEventListener('mousemove', this.onMove)
   }
 
   onMove(event) {
+    const finalPosition = this.updatePosition(event.clientX)
+    this.moveSlide(finalPosition)
   }
 
   onEnd(event) {
     this.wrapper.removeEventListener('mousemove', this.onMove)
+    this.dist.finalPosition = this.dist.movePosition
   }
 
   addSlideEvents() {
