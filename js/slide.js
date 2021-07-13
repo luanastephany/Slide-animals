bdd45512880acc8d7c46d5a2009b24820067423b
Luana Stephany <ls.luanastephany@gmail.com>
2021-07-13 16:45:10 -0300

    slide next e prev, e transition

--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,11 @@ export default class Slide {
     }
   }
 
+  //transition 
+  transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : ''
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX
     this.slide.style.transform = `translate3d(${distX}px, 0, 0)`
@@ -31,6 +36,7 @@ export default class Slide {
       movetype = 'touchmove'
     }
     this.wrapper.addEventListener(movetype, this.onMove)
+    this.transition(false)
   }
 
   onMove(event) {
@@ -43,6 +49,18 @@ export default class Slide {
     const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
     this.wrapper.removeEventListener(movetype, this.onMove)
     this.dist.finalPosition = this.dist.movePosition
+    this.transition(true)
+    this.changeSlideOnEnd()
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide()
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide()
+    } else {
+      this.changeSlide(this.index.active)
+    }
   }
 
   addSlideEvents() {
@@ -74,13 +92,38 @@ export default class Slide {
     })
   }
 
+  slidesIndexNav(index) {
+    const last = this.slideArray.length - 1
+    this.index = {
+      prev: index ? index - 1 : undefined,
+      active: 0,
+      next: index === last ? undefined : index + 1,
+    }
+  }
+
   changeSlide(index) {
-    this.moveSlide(this.slideArray[index].position)
+    const activeSlide = this.slideArray[index]
+    this.moveSlide(activeSlide.position)
+    this.slidesIndexNav(index)
+    this.dist.finalPosition = activeSlide.position
+  }
+
+  activePrevSlide() {
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev)
+    }
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next)
+    }
   }
 
 
   init() {
     this.bindEvents()
+    this.transition(true)
     this.addSlideEvents()
     this.slidesConfig()
     return this
